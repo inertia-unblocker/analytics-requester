d57817c1b2553afbb82b5040b1b82b3eb088643c
Angad Tendulkar <angad.tendulkar@gmail.com>
2022-04-02 18:26:19 -0400

    use final formatted data instead

--- index.js
+++ index.js
@@ -51,8 +51,66 @@ app.get('/getData', async (req, res) => {
 		let stats = await get(`${umamiUrl}/api/website/${website_id}/stats`, { start_at: getUnixTime(new Date(now.toISOString().substring(10, 0))), end_at: getUnixTime(now) }, headers);
 		let pageviews = await get(`${umamiUrl}/api/website/${website_id}/pageviews`, { start_at: getUnixTime(new Date(now.toISOString().substring(10, 0))), end_at: getUnixTime(now), unit: 'hour', tz: 'America/New_York' }, headers);
 
-		res.status(200).json({ stats, pageviews });
+		let data = { stats, pageviews };
+
+		let finalData = {
+			columns_daily: [
+				{
+					key: 'name',
+					label: 'Today',
+				},
+				{
+					key: 'value',
+					label: '',
+				}
+			],
+			rows_daily: [
+				{
+					key: '1',
+					name: 'Bounces',
+					value: data.stats.bounces,
+				},
+				{
+					key: '2',
+					name: 'Pageviews',
+					value: data.stats.pageviews,
+				},
+				{
+					key: '3',
+					name: 'Total Time',
+					value: data.stats.totaltime,
+				},
+				{
+					key: '4',
+					name: 'Visitors',
+					value: data.stats.uniques,
+				}
+			],
+			columns_hourly: [
+				{
+					key: 'time',
+					label: 'Hourly',
+				},
+				{
+					key: 'value',
+					label: '',
+				}
+			],
+			rows_hourly: data.pageviews.sessions.length > 0 ? data.pageviews.sessions.forEach((elem, index) => {
+				let time = convertTimeToTimeZone(elem.t);
+				let value = elem.y;
+	
+				return {
+					key: index+1,
+					time: time,
+					value: value,
+				};
+			}) : [],
+		};
+
+		res.status(500).json(finalData);
 	} catch (err) {
+		console.log(err);
 		res.status(500).json({ error: err.message });
 	}
 });
