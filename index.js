6078e566607f8ca70cddb7b2f8a2e33fac3335ee
Angad Tendulkar <angad.tendulkar@gmail.com>
2022-04-04 17:25:09 -0400

    [forever fix] tz

--- index.js
+++ index.js
@@ -13,150 +13,156 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-function getUnixTime(dateObject) {
-	return Math.floor(dateObject.getTime());
-}
-
-function convert(unixTime, tz) {
-	let hr = 3600000;
-	let min = 60000;
-	let offset = timezones.filter(timezone => timezone.abbr == tz)[0].offset;
-
-	let hroffset = Math.floor(offset * hr);
-	let minoffset = 0;
-
-	if (offset.toString().endsWith('.5')) {
-		minoffset = min * 30;
-	}
-
-	if (offset.toString().endsWith('.75') /* **cough** NEPAL **cough** */) {
-		minoffset = min * 45;
-	}
-
-	let returnTime = (unixTime-hroffset)-minoffset;
-	return returnTime;
-}
-
-function from24to12(hmsString) {
-	[h, m, s] = hmsString.split(':');
-	h = parseInt(h);
-	m = parseInt(m);
-	s = parseInt(s);
-	let ap = '';
-
-	if (h > 12) {
-		h -= 12;
-		ap = 'PM';
-	} else if (h < 12) {
-		ap = 'AM';
-	} else {
-		ap = 'PM';
-	}
+TimeDate = class timeDateUtils {
+	convertSec(sec) {
+		let wk = 0;
+		let day = 0;
+		let hr = 0;
+		let min = 0;
 	
-	h = h.toString();
-	m = m.toString();
-	s = s.toString();
-
-	m = m.length == 1 ? '0' + m : m;
-	s = s.length == 1 ? '0' + s : s;
-
-	return `${h}:${m} ${ap}`;
-}
-
-function convertSec(sec) {
-	let wk = 0;
-	let day = 0;
-	let hr = 0;
-	let min = 0;
-
-	while (sec >= 60) {
-		min += 1;
-		sec -= 60;
+		while (sec >= 60) {
+			min += 1;
+			sec -= 60;
+		}
+	
+		while (min >= 60) {
+			hr++;
+			min -= 60;
+		}
+	
+		while (hr >= 24) {
+			day++;
+			hr -= 24;
+		}
+	
+		while (day >= 7) {
+			wk++;
+			day -= 7;
+		}
+	
+		let wkstr = wk ? `${wk}w, ` : '';
+		let daystr = day ? `${day}d, ` : '';
+		let hrstr = hr ? `${hr}` : '00';
+		let minstr = min ? `${min}` : '00';
+		let secstr = sec ? `${sec}` : '00';
+	
+		hrstr  = hrstr.length  == 1 ? '0' + hrstr : hrstr;
+		minstr = minstr.length == 1 ? '0' + minstr : minstr;
+		secstr = secstr.length == 1 ? '0' + secstr : secstr;
+	
+		return `${wkstr}${daystr}${hrstr}h ${minstr}m ${secstr}s`;
 	}
 
-	while (min >= 60) {
-		hr++;
-		min -= 60;
+	from24to12(hmsString) {
+		let hms = hmsString.split(':');
+		
+		let h = parseInt(hms[0]);
+		let m = parseInt(hms[1]);
+		let s = parseInt(hms[2]);
+		let ap = '';
+	
+		if (h > 12) {
+			h -= 12;
+			ap = 'PM';
+		} else if (h < 12) {
+			ap = 'AM';
+		} else {
+			ap = 'PM';
+		}
+		
+		h = h.toString();
+		m = m.toString();
+		s = s.toString();
+	
+		m = m.length == 1 ? '0' + m : m;
+		s = s.length == 1 ? '0' + s : s;
+	
+		return `${h}:${m} ${ap}`;
 	}
 
-	while (hr >= 24) {
-		day++;
-		hr -= 24;
+	getTZData(tz) {
+		let tzData = '';
+		
+		try { tzData = timezones.filter(timezone => timezone.abbr == tz)[0]; }
+		catch (e) { 
+			tz = tz.replace('GMT', 'UTC');
+			tz = tz.replace('UTC', '');
+			
+			tzData = timezones.filter(timezone => timezone.offset == parseInt(tz))[0]; 
+		}
+	
+		return tzData;
 	}
 
-	while (day >= 7) {
-		wk++;
-		day -= 7;
+	getStartEndTime(tzData) {
+		const dateObj = new Date();
+
+		let date = dateObj.getDate();
+		let month = dateObj.getMonth() + 1;
+		let year = dateObj.getFullYear();
+
+		let thisMorning = new Date(`${year}-${month}-${date} 00:00:00 ${tzData.abbr}`);
+		let thisEvening = new Date(`${year}-${month}-${date} 23:59:59 ${tzData.abbr}`);
+
+		let thisMorningMS = thisMorning.getTime();
+		let thisEveningMS = thisEvening.getTime();
+
+		return {thisMorningMS, thisEveningMS};
+	}
+};
+
+Net = class networkUtils {
+	request(url, params = {}, headers, method = 'GET') {
+		let options = {
+			method,
+			headers,
+		};
+		
+		if (method == 'GET') {
+			url += '?' + (new URLSearchParams(params)).toString();
+		} else if (method == 'POST') {
+			options.body = JSON.stringify(params);
+		}
+		
+		return fetch(url, options).then(response => response.json());
 	}
 
-	wkstr = wk ? `${wk}w, ` : '';
-	daystr = day ? `${day}d, ` : '';
-	hrstr = hr ? `${hr}` : '00';
-	minstr = min ? `${min}` : '00';
-	secstr = sec ? `${sec}` : '00';
+	get(url, params, headers) {
+		return this.request(url, params, headers, 'GET');
+	}
 
-	hrstr  = hrstr.length  == 1 ? '0' + hrstr : hrstr;
-	minstr = minstr.length == 1 ? '0' + minstr : minstr;
-	secstr = secstr.length == 1 ? '0' + secstr : secstr;
+	post(url, params, headers) {
+		return this.request(url, params, headers, 'POST');
+	}
+};
 
-	return `${wkstr}${daystr}${hrstr}h ${minstr}m ${secstr}s`;
-}
-
-
+const timeDate = new TimeDate();
+const net = new Net();
 
 app.options('/getData', cors());
 app.get('/getData', async (req, res) => {
 	try {
-		let tz = req.query.tz;
-
-		const request = (url, params = {}, headers, method = 'GET') => {
-			let options = {
-				method,
-				headers,
-			};
-			
-			if (method == 'GET') {
-				url += '?' + (new URLSearchParams(params)).toString();
-			} else if (method == 'POST') {
-				options.body = JSON.stringify(params);
-			}
-			
-			return fetch(url, options).then(response => response.json());
-		};
+		// ======== BOILERPLATE ======== //
+		const tzData = timeDate.getTZData(req.query.tz);
+		const {thisMorningMS, thisEveningMS} = timeDate.getStartEndTime(tzData);
 		
-		const get = (url, params, headers) => request(url, params, headers, 'GET');
-		const post = (url, params, headers) => request(url, params, headers, 'POST');
 
-		const now = new Date();
-		
-		let jsonres_token = await post(`${umamiUrl}/api/auth/login`, { username: umamiUsername, password: umamiPassword });
-		const token = jsonres_token.token;
+		// ======== DATA ======== //
+		const token = (await net.post(`${umamiUrl}/api/auth/login`, { username: umamiUsername, password: umamiPassword })).token;
+		const headers = { 'Authorization': `Bearer ${token}`};
 
-		let headers = {
-			'Authorization': `Bearer ${token}`
-		};
+		const websites = await net.get(`${umamiUrl}/api/websites`, {}, headers);
+		const website_id = websites.filter(website => website.name.toLowerCase() === 'inertia')[0].website_id;
 
-		let jsonres_websites = await get(`${umamiUrl}/api/websites`, {}, headers);
-		let website_id = jsonres_websites.filter(website => website.name.toLowerCase() === 'inertia')[0].website_id;
-
-		thismorning = getUnixTime(new Date(now.toISOString().substring(10, 0)));
-
-		let stats = await get(`${umamiUrl}/api/website/${website_id}/stats`, { start_at: convert(thismorning, tz), end_at: convert(getUnixTime(now), tz) }, headers);
-		let pageviews = await get(`${umamiUrl}/api/website/${website_id}/pageviews`, { start_at: convert(thismorning, tz), end_at: convert(getUnixTime(now), tz), unit: 'hour', tz: 'America/New_York' }, headers);
-
-
-
-
-
-
-
+		let stats = await net.get(`${umamiUrl}/api/website/${website_id}/stats`, {start_at: thisMorningMS, end_at: thisEveningMS }, headers);
+		let pageviews = await net.get(`${umamiUrl}/api/website/${website_id}/pageviews`, { start_at: thisMorningMS, end_at: thisEveningMS, unit: 'hour', tz: 'America/New_York' }, headers);
 
 
 		// ======= FORMATTING ======= //
 		let data = { stats, pageviews };
 		let rows_hourly = [];
 		for (let i=0; i<data.pageviews.pageviews.length; i++) {
-			let arrayDate = from24to12(data.pageviews.pageviews[i].t.split(' ')[1]);
+			let arrayDate = timeDate.from24to12(data.pageviews.pageviews[i].t.split(' ')[1]);
 			let arrayValue = data.pageviews.pageviews[i].y;
 	
 			rows_hourly.push({
@@ -166,7 +172,7 @@ app.get('/getData', async (req, res) => {
 			});
 		}
 
-		let totaltime = convertSec(parseInt(data.stats.totaltime.value));
+		let totaltime = timeDate.convertSec(parseInt(data.stats.totaltime.value));
 
 		let finalData = {
 			columns_daily: [
@@ -222,7 +228,7 @@ app.get('/getData', async (req, res) => {
 });
 
 app.get('/', (req, res) => {
-	res.redirect('/getData');
+	res.redirect(`/getData?tz=${req.query.tz}`);
 });
 
 app.listen(PORT, () => {
